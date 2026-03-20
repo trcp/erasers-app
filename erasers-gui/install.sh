3821d30c4107acb9b8bf6a6a7f39d02bf55bc11c
roboworks <roboworks@trcp-katana-1>
2026-03-20 19:04:16 +0900

    fix pash

--- erasers-gui/install.sh
+++ erasers-gui/install.sh
@@ -10,7 +10,7 @@ docker build -t erasers:gui . || die "Docker build failed"
 log "Docker image built successfully"
 
 # systemd service
-SERVICE_SRC="$HOME/erasers-app/erasers-gui/erasers.gui.service"
+SERVICE_SRC="/home/hsr-hmi/erasers-app/erasers-gui/erasers.gui.service"
 SERVICE_DST="/etc/systemd/system/erasers.gui.service"
 
 [ -f "$SERVICE_SRC" ] || die "Service file not found: $SERVICE_SRC"
@@ -21,7 +21,7 @@ sudo systemctl enable erasers.gui.service || die "Failed to enable service"
 log "systemd service enabled"
 
 # Autostart
-AUTOSTART_DIR="$HOME/.config/autostart"
+AUTOSTART_DIR="/home/hsr-hmi/.config/autostart"
 AUTOSTART_FILE="$AUTOSTART_DIR/erasers-gui.desktop"
 
 log "Configuring autostart..."
@@ -30,7 +30,7 @@ cat > "$AUTOSTART_FILE" <<EOF
 [Desktop Entry]
 Type=Application
 Name=Erasers GUI
-Exec=/usr/bin/chromium-browser --password-store=basic --kiosk --incognito --disable-features=Translate -disk-cache-size=1 -media-cache-size=1 http://localhost:3000
+Exec=/usr/bin/chromium-browser --password-store=basic --kiosk --incognito --disable-features=Translate -disk-cache-size=1 -media-cache-size=1 http://localhost:5173
 Hidden=false
 NoDisplay=false
 X-GNOME-Autostart-enabled=true
